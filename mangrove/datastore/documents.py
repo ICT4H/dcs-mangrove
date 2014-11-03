52149143ff203f0d0bfdfb27c0207e22a10bc104
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-11-03 17:03:03 +0530

    Prince |  added new field to form_model(created_through)

--- mangrove/datastore/documents.py
+++ mangrove/datastore/documents.py
@@ -156,6 +156,7 @@ class FormModelDocument(DocumentBase):
     validators = ListField(DictField())
     snapshots = DictField()
     xform = TextField()
+    created_through = TextField()
 
     def __init__(self, id=None):
         DocumentBase.__init__(self, id=id, document_type='FormModel')
