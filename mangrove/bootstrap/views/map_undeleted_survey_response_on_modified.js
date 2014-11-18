ef4d8dee0bdb3e63e18810b7b757bac51ddeed75
Prince M Jain <princemj@thoughtworks.com>
2014-11-18 16:41:50 +0530

    Prince | creating new view for delta feature

---
+++ mangrove/bootstrap/views/map_undeleted_survey_response_on_modified.js
@@ -0,0 +1,8 @@
+function(doc) {
+    var isNotNull = function(o) {
+        return !((o === undefined) || (o == null));
+    };
+    if (doc.document_type == 'SurveyResponse' && isNotNull(doc.form_model_id) && !doc.void) {
+        emit([doc.form_model_id, Date.parse(doc.modified)], doc);
+    }
+}
